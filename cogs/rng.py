7ef0b82abc19f5786034836a285c9a976eb77e5f
Ardai Daniel <pirill_poveniy@yahoo.com>
2017-06-17 13:21:17 +0300

    Implemented roll command properly. I hope.

--- cogs/rng.py
+++ cogs/rng.py
@@ -1,34 +1,59 @@
 from discord.ext import commands
 import random
-import re
+import re as regex
 
 class RNG():
     #initialize class
     def __init__(self, bot):
         self.bot = bot
 
+    #Converts a number to a string in order to display it using emoji
+    def convertToString(self, value):
+        list = [':zero:',':one:', ':two:', ':three:', ':four:', ':five:', ':six:', ':seven:', ':eight:', ':nine:']
+        message = ''
+
+        string = str(value)
+
+        for c in string:
+            message = message + list[int(c)]
+
+        return message
+
     @commands.command()
     async def roll(self, message: str = "1d6"):
         """
-            Displays a random die value.
-            Optional parameter allows for rolling of more dice (maximum 20).
-            Examples: !roll | !roll 3
+            Displays a random d6 value.
+            Optional parameter allows for rolling dice using XdY dice notation (maximum 20 dice)
+            Examples: !roll | !roll 3d10 | !roll 10D4
         """
+        pattern = regex.compile('^(\d+)(d|D)(\d+)$')
+        if regex.match(pattern, message):
+            #Process string to obtain int values
+            message = message.replace('D','d')
+            numDice, diceValue = message.split('d')
+            numDice = int(numDice)
+            diceValue = int(diceValue)
 
-        pattern = re.compile('^(\d+)d(\d+)$')
-        if re.match(pattern, message):
-            await self.bot.say('correct format: {0}'.format(message))
+            #Limit the maximum number of dice
+            if numDice > 20:
+                await self.bot.say('Too many dice! Maximum number is 20.')
+                return
+            else:
+                returnMessage = 'Rolled {0}:  '.format(message)
+
+                for i in range(0, numDice - 1):
+                    value = random.randint(1, diceValue)
+                    returnMessage = returnMessage + self.convertToString(value) + " - "
+
+                #Last value shouldn't display a separator
+                value = random.randint(1, diceValue)
+                returnMessage = returnMessage + self.convertToString(value)
+
+                await self.bot.say(returnMessage)
+
         else:
-            await self.bot.say('wrong format: {0}'.format(message))
-
-       # if dice < 1 or dice > 20:
-       #     await self.bot.say('Invalid number of dice.')
-       #     return
-       #
-       # for i in range(0, dice):
-       #     message = message + random.choice([
-       #        ":one:,",":two:,",":three:,",":four:,",":five:,",":six:,"
-       #     ])
+            await self.bot.say('wrong format: {0} Please use dice notation. (XdY)'.format(message))
+            return
 
 
     #error handler for the poot command
